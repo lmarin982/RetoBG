c762674f2b9e02971a9baaa12ef0cfb503b0736d
lmarin982 <lmarin@biclout.com>
2025-02-15 12:44:18 -0500

    Actualizar archivos de caché y rutas de proyecto en la solución

--- backend/src/reto_bg.Domain/obj/Debug/net9.0/reto_bg.Domain.AssemblyInfo.cs
+++ backend/src/reto_bg.Domain/obj/Debug/net9.0/reto_bg.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reto_bg.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af853657a530305a3cb03850d01b87141c61b355")]
 [assembly: System.Reflection.AssemblyProductAttribute("reto_bg.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reto_bg.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
